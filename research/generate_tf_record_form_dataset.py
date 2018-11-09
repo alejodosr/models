cb146a8d429cee0e39ba40002ce992694b79d444
alejodosr <alejandro.rramos@alumnos.upm.es>
2018-11-09 14:22:02 -0500

    fixed bugs in the width and size of tf record

--- research/generate_tf_record_form_dataset.py
+++ research/generate_tf_record_form_dataset.py
@@ -12,8 +12,8 @@ from object_detection.dataset_tools import tf_record_creation_util
 
 # Rootdir of the dataset
 #rootdir = "/media/alejandro/DATA/datasets/simdronet"
-ROOTDIR = "/media/alejandro/DATA/datasets/simple_uav_dataset/simulation_divided/eval"
-OUTPATH = "/media/alejandro/DATA/datasets/simple_uav_dataset/simulation_divided/uav_eval.record"
+ROOTDIR = "/media/alejandro/DATA/datasets/simple_uav_dataset/simulation_divided/val"
+OUTPATH = "/media/alejandro/DATA/datasets/simple_uav_dataset/simulation_divided/uav_val.record"
 
 SHOW_IMAGES = 0
 WRITE_ANNOTATIONS = 0
@@ -22,7 +22,8 @@ ENABLE_SHARDING = 1
 
 if ENABLE_SHARDING:
     num_shards = 10
-    index = 0
+
+index_shar = 0
 
 if not ENABLE_SHARDING:
     # Create writer
@@ -43,12 +44,14 @@ with contextlib2.ExitStack() as tf_record_close_stack:
                 file_seg = "frame_seg_" + result + ".jpg"
                 print(file_seg)
 
-                if ENABLE_SHARDING:
-                    index += 1
+                index_shar += 1
 
 
                 # Load an color image in grayscale
                 img = cv2.imread(os.path.join(subdir, file))
+                img_height, img_width = img.shape[:2]
+                # print(str(img_width) +" "+str(img_height))
+                # input()
                 # Load an color image in grayscale
                 img_seg = cv2.imread(os.path.join(subdir, file_seg))
                 mask = cv2.cvtColor(img_seg, cv2.COLOR_BGR2GRAY)
@@ -57,7 +60,7 @@ with contextlib2.ExitStack() as tf_record_close_stack:
                 # img = cv2.bitwise_and(img, img, mask=mask)
 
                 x, y, w, h = cv2.boundingRect(mask)
-                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
                 if WRITE_ANNOTATIONS:
                     if not GENERATE_FOR_COLAB:
@@ -73,20 +76,29 @@ with contextlib2.ExitStack() as tf_record_close_stack:
                             the_file.write(str(temp + "/" + file) + "," + str(x) + "," + str(y) + "," + str(x + w) + ","
                                            + str(y + h) + "," + "drone" + '\n')
 
-                height = h # Image height
-                width = w  # Image width
-                filename = tf.compat.as_bytes(file)  # Filename of the image. Empty if image is not from file
+                # print(str(x))
+                # print(str(y))
+                # print(str(w))
+                # print(str(h))
+
+                height = img_height # Image height
+                width = img_width  # Image width
+                filename = tf.compat.as_bytes(str(index_shar).zfill(6) + "_" + file)  # Filename of the image. Empty if image is not from file
                 encoded_image_data = tf.compat.as_bytes(cv2.imencode('.jpg', img)[1].tostring())  # Encoded image bytes
                 image_format = b'jpg'  # b'jpeg' or b'png'
 
-                xmins = [float(x) / float(w)]  # List of normalized left x coordinates in bounding box (1 per box)
-                xmaxs = [float(x + w) / float(w)]  # List of normalized right x coordinates in bounding box
+                xmins = [float(x) / float(img_width)]  # List of normalized left x coordinates in bounding box (1 per box)
+                xmaxs = [float(x + w) / float(img_width)]  # List of normalized right x coordinates in bounding box
                 # (1 per box)
-                ymins = [float(y) / float(h)]  # List of normalized top y coordinates in bounding box (1 per box)
-                ymaxs = [float(y + h) / float(h)]  # List of normalized bottom y coordinates in bounding box
+                ymins = [float(y) / float(img_height)]  # List of normalized top y coordinates in bounding box (1 per box)
+                ymaxs = [float(y + h) / float(img_height)]  # List of normalized bottom y coordinates in bounding box
                 # (1 per box)
                 classes_text = [tf.compat.as_bytes('Drone')]  # List of string class name of bounding box (1 per box)
                 classes = [1]  # List of integer class id of bounding box (1 per box)
+
+                if (xmins[0] > 1.1 or ymins[0] > 1.1 or xmaxs[0] > 1.1 or ymaxs[0] > 1.1):
+                    print("Higher " + str(xmins[0]) + " " +  str(ymins[0]) + " " +  str(xmaxs[0]) + " " + str(ymaxs[0]))
+                    input()
 
                 tf_example = tf.train.Example(features=tf.train.Features(feature={
                     'image/height': dataset_util.int64_feature(height),
@@ -107,7 +119,7 @@ with contextlib2.ExitStack() as tf_record_close_stack:
                     # Write tf example
                     writer.write(tf_example.SerializeToString())
                 else:
-                    output_shard_index = index % num_shards
+                    output_shard_index = index_shar % num_shards
                     output_tfrecords[output_shard_index].write(tf_example.SerializeToString())
 
                 if SHOW_IMAGES:
